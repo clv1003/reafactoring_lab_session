cf4bf3e387af7686f01dc3a12d81c41af58c0f27
Claudia Landeira Viñuela <clv1003@alu.ubu.es>
2023-05-18 18:34:35 +0200

    Tarea 20. Ajuste de la invocación de WorkStation y Printer.

--- src/lanSimulation/internals/WorkStation.java
+++ src/lanSimulation/internals/WorkStation.java
@@ -1,5 +1,13 @@
 package lanSimulation.internals;
 
-public class WorkStation {
+public class WorkStation extends Node {
 
+	public WorkStation(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+	}
+	
+	public WorkStation(byte type, String name) {
+		super(type, name);
+	}
+	
 }
